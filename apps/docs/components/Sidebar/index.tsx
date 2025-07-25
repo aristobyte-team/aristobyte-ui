d032a1a9df3c90f5446d993a90885bd0944c9851
aristobyte <info@aristobyte.com>
2025-07-25 16:31:04 +0200

    feat: get rid of the utils folder and move everything to config in apps/docs

--- apps/docs/components/Sidebar/index.tsx
+++ apps/docs/components/Sidebar/index.tsx
@@ -3,7 +3,7 @@
 import * as React from "react";
 import Link from "next/link";
 import { useConfig, useTranslate } from "@/context";
-import { Icons } from "@/utils";
+import { Icons } from "@/config";
 
 import "./Sidebar.scss";
 
--- apps/docs/utils/index.ts
+++
@@ -1,3 +0,0 @@
-export { Icons } from "./icons";
-export { CodeBlocks } from "./codeBlocks";
-export { PreviewBlocks } from "./previewBlocks";
