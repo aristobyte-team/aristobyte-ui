ffccfb59778de5cef71c5a7f2d18b5407fb6709e
aristobyte <info@aristobyte.com>
2025-08-30 21:54:19 +0200

    feat: add a common component intro for each section - however not unified yet

--- apps/docs/components/Layout/SectionMapping/CardSections/Intro.tsx
+++ apps/docs/components/Layout/SectionMapping/CardSections/Intro.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 
-import { Anchor } from "@aristobyte-ui/anchor";
+import { Intro as IntroComponent } from "@/components";
 
 import { type ConfigType } from "@/config";
 import { type TranslateFunctionType } from "@/data";
@@ -11,12 +11,5 @@ export type IntroPropsType = {
 };
 
 export const Intro = ({ config, t }: IntroPropsType) => (
-  <div className="layout__links">
-    {config.introLinks.map(({ id, href, target, icon }) => (
-      <Anchor className="layout__link" key={id} href={href} target={target}>
-        <span dangerouslySetInnerHTML={{ __html: icon }} />
-        <span>{t(`layout.components.card.intro.links.${id}`)}</span>
-      </Anchor>
-    ))}
-  </div>
+  <IntroComponent pkg="card" config={config} t={t} />
 );
