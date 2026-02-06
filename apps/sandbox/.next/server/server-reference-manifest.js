ef8f55d546d5472c0d5b7983bf431d52c86c42e1
aristobyte <info@aristobyte.com>
2026-02-06 18:21:50 +0100

    chore: remove .next in sandbox folder from remote

--- apps/sandbox/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"NPq4XmORPq78OY7smPnTrlO/3FJrB+RRpEZO6p7ot8I=\"\n}"
--- apps/sandbox/.next/static/chunks/apps_sandbox_app_layout_tsx_0a548d63._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/apps_sandbox_app_f9886e06._.css",
-  "static/chunks/node_modules_next_dist_8db7fb1f._.js"
-],
-    source: "dynamic"
-});
